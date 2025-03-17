30aae6654b7ce9ed3a12f6d4e4c7bb77cea59b8d
Xunevere <2823822289@qq.com>
2025-03-17 11:16:55 +0800

    v2.1

--- TDD.py
+++ TDD.py
@@ -6,34 +6,67 @@
 
 # assert 'Django' in browser.page_source
 
-#2
+# #2
+# from selenium import webdriver
+# browser = webdriver.Chrome()
+
+# # 张三听说有一个在线待办事项的应用
+# # 他去看了这个应用的首页
+# browser.get('http://localhost:8000')
+
+# # 他注意到网页里包含了“To-Do”这个词
+# assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+# # 应用有一个输入待办事项的文本输入框
+
+# # 他在文本输入框中输入了“Buy flowers”
+
+# # 他按下回车键后，页面更新了
+# # 待办事项表格中显示了“1: Buy flowers”
+
+# # 页面中又显示了一个文本输入框，可以输入其他待办事项
+
+# # 他输入了“Send a gift to Lisi”，并按下回车键
+
+# # 页面再次更新，他的清单中显示了这两个待办事项
+
+# # 张三想知道这个网站是否会记住他的清单
+# # 他看到网站为他生成了一个唯一的URL
+
+# # 他访问那个URL，发现待办事项列表还在
+# # 他很满意地离开了
+
+# browser.quit()
+
+#2.1
 from selenium import webdriver
-browser = webdriver.Chrome()
+import unittest
 
-# 张三听说有一个在线待办事项的应用
-# 他去看了这个应用的首页
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# 他注意到网页里包含了“To-Do”这个词
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-# 应用有一个输入待办事项的文本输入框
+    def tearDown(self):
+        self.browser.quit()
 
-# 他在文本输入框中输入了“Buy flowers”
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        
+        # 张三听说有一个在线待办事项的应用
+        # 他去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
 
-# 他按下回车键后，页面更新了
-# 待办事项表格中显示了“1: Buy flowers”
+        # 他注意到网页里包含了“To-Do”这个词
+        # self.assertIn('To-Do', self.browser.title), "Browser title was " + self.browser.title
+        self.assertIn('To-Do', self.browser.title, "Browser title was " + self.browser.title)
+        self.fail('Finish the test!')
 
-# 页面中又显示了一个文本输入框，可以输入其他待办事项
+        # 应用有一个输入待办事项的文本输入框
 
-# 他输入了“Send a gift to Lisi”，并按下回车键
+        # 他在文本输入框中输入了“Buy flowers”
 
-# 页面再次更新，他的清单中显示了这两个待办事项
+        # 他访问那个URL，发现待办事项列表还在
+        # 他很满意地离开了
 
-# 张三想知道这个网站是否会记住他的清单
-# 他看到网站为他生成了一个唯一的URL
-
-# 他访问那个URL，发现待办事项列表还在
-# 他很满意地离开了
-
-browser.quit()
+if __name__ == '__main__':
+    unittest.main()
