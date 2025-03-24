c1af9593e20e9e539a2a6f11b7d2bf926a6f006d
Xunevere <2823822289@qq.com>
2025-03-24 10:23:27 +0800

    message 4: First unit test

---
+++ lists/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page #(1)
+
+class HomePageTest(TestCase): 
+    def test_root_url_resolves_to_home_page_view(self): 
+        found = resolve('/') #(2)
+        self.assertEqual(found.func, home_page) #(3)
