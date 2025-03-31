440e2d3d8946173716c6e96d308f990311093edb
Xunevere <2823822289@qq.com>
2025-03-31 10:28:43 +0800

    message 6: Functional test now checks we can input a to-do item

--- TDD.py
+++ TDD.py
@@ -1,46 +1,10 @@
-# #1
-# from selenium import webdriver
 
-# browser = webdriver.Chrome()
-# browser.get('http://localhost:8000')
 
-# assert 'Django' in browser.page_source
-
-# #2
-# from selenium import webdriver
-# browser = webdriver.Chrome()
-
-# # 张三听说有一个在线待办事项的应用
-# # 他去看了这个应用的首页
-# browser.get('http://localhost:8000')
-
-# # 他注意到网页里包含了“To-Do”这个词
-# assert 'To-Do' in browser.title, "Browser title was " + browser.title
-
-# # 应用有一个输入待办事项的文本输入框
-
-# # 他在文本输入框中输入了“Buy flowers”
-
-# # 他按下回车键后，页面更新了
-# # 待办事项表格中显示了“1: Buy flowers”
-
-# # 页面中又显示了一个文本输入框，可以输入其他待办事项
-
-# # 他输入了“Send a gift to Lisi”，并按下回车键
-
-# # 页面再次更新，他的清单中显示了这两个待办事项
-
-# # 张三想知道这个网站是否会记住他的清单
-# # 他看到网站为他生成了一个唯一的URL
-
-# # 他访问那个URL，发现待办事项列表还在
-# # 他很满意地离开了
-
-# browser.quit()
-
-#2.1
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -56,17 +20,37 @@ class NewVisitorTest(unittest.TestCase):
         # 他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
-        # 他注意到网页里包含了“To-Do”这个词
-        # self.assertIn('To-Do', self.browser.title), "Browser title was " + self.browser.title
-        self.assertIn('To-Do', self.browser.title, "Browser title was " + self.browser.title)
-        self.fail('Finish the test!')
+        # 他注意到网页的标签和头部都包含了“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text #(1)
+        self.assertIn('To-Do', header_text) 
+    
 
         # 应用有一个输入待办事项的文本输入框
+        inputbox = self.browser.find_element(By.ID,'id_new_item') #(1)
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+            )   
+ 
 
         # 他在文本输入框中输入了“Buy flowers”
+        inputbox.send_keys('Buy flowers') #(2)
 
-        # 他访问那个URL，发现待办事项列表还在
-        # 他很满意地离开了
+        #他按了回车键后，页面更新了
+        #待办事项表格中显示了“1: Buy flowers”
+        inputbox.send_keys(Keys.ENTER) #(3)
+        time.sleep(1) #（4）
+
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr') #(1)
+        self.assertIn('1: Buy flowers', [row.text for row in rows]) 
+
+
+        #页面中又显示了一个文本输入框，可以输入其他待办事项
+        #他输入了“gift to girlfriend”
+        self.fail('Finish the test!') 
+        #页面再次更新，他的清单中显示了这两个待办事项
 
 if __name__ == '__main__':
     unittest.main()
