bbd2ad2e6985c69f07ad50cb566c2e85ca7519c4
Xunevere <2823822289@qq.com>
2025-03-31 11:07:03 +0800

    message 8 :Before refactoring the functional_tests

--- lists/views.py
+++ lists/views.py
@@ -5,6 +5,8 @@
 #     return HttpResponse('<html><title>To-Do lists</title></html>')
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')})
