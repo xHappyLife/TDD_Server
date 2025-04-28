c6126135e59b8b85db3b90a21f347476ebfb5b36
Xunevere <2823822289@qq.com>
2025-04-28 19:45:17 +0800

    message 14.1 just in case

--- lists/tests.py
+++ lists/tests.py
@@ -25,18 +25,6 @@ class ItemModelTest(TestCase):
 
 
 class HomePageTest(TestCase): 
-    # def test_root_url_resolves_to_home_page_view(self): 
-    #     found = resolve('/') 
-    #     self.assertEqual(found.func, home_page) 
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response = self.client.get('/')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
-
     def test_use_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
@@ -51,18 +39,18 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})     
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-new-page/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+class ListViewTest(TestCase):
+   def test_displays_all_items(self):
+       Item.objects.create(text='itemey 1')
+       Item.objects.create(text='itemey 2')
 
+       response = self.client.get('/lists/the-new-page/')
 
-    # def test_home_page_returns_correct_html(self):
-    #     request = HttpRequest() #(1)
-    #     response = home_page(request) #(2)
-    #     html = response.content.decode('utf8') #(3)
-    #     self.assertTrue(html.startswith('<html>')) #(4)
-    #     self.assertIn('<title>To-Do lists</title>', html) #(5)
-    #     self.assertTrue(html.endswith('</html>')) #(6)
+       self.assertContains(response, 'itemey 1')
+       self.assertContains(response, 'itemey 2')
