7f842d8df5929c68ebe597d3df4675cdfe17fc4e
Xunevere <2823822289@qq.com>
2025-04-29 17:24:23 +0800

    message 20: testing for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -114,6 +114,20 @@ class NewVisitorTest(LiveServerTestCase):
 
         #两人都很满意，然后去睡觉了
 
+    def test_layout_and_styling(self):
+        # 张三访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #他看到输入框完美地居中显示
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+
 
 # if __name__ == '__main__':
 #     unittest.main()
